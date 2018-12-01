697cadd22c28d0d5aebbae85cd47478711e95236
AugChao <fychao.tw@gmail.com>
2018-12-01 14:24:38 +0800

    加上 ucan 資料集

--- back-end/app.py
+++ back-end/app.py
@@ -1,6 +1,9 @@
 import flask
 import re
+import os
+import random
 import dill
+import json
 import jieba
 from sklearn.cluster import KMeans
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -40,13 +43,74 @@ def hello():
     input_vector = tfidf.transform([token(mstr)])
     return "%s"%kmeans.predict(input_vector)
 
+
+
+
+def sort_coo(coo_matrix):
+    tuples = zip(coo_matrix.col, coo_matrix.data)
+    return sorted(tuples, key=lambda x: (x[1], x[0]), reverse=True)
+ 
+def extract_topn_from_vector(feature_names, sorted_items, topn=10):
+    """get the feature names and tf-idf score of top n items"""
+    
+    #use only topn items from vector
+    sorted_items = sorted_items[:topn]
+ 
+
+    score_vals = []
+    feature_vals = []
+    
+    # word index and corresponding tf-idf score
+    for idx, score in sorted_items:
+        
+        #keep track of feature name and its corresponding score
+        score_vals.append(round(score, 3))
+        feature_vals.append(feature_names[idx])
+ 
+    return zip(feature_vals, score_vals)
+
+def getJobFeature(fn_num=5):
+	bdir = "./jobs/"
+	# 以下為 ucan 文本集合使用
+	#bdir = "./ucan/"
+	pick_files = random.sample(os.listdir(bdir), fn_num)
+
+	cnts = [ json.loads(open(bdir+pick_file, 'r', encoding='utf8').read())['job_desc'] for pick_file in pick_files]
+
+	# 以下為 ucan 文本集合使用
+	#cnts = [ open(bdir+pick_file, 'r', encoding='utf8').read() for pick_file in pick_files]
+
+	ans = tfidf.transform([token(cnt) for cnt in cnts])
+
+	feature_names=tfidf.get_feature_names()
+
+	#sort the tf-idf vectors by descending order of scores
+	sorted_items=sort_coo(ans.tocoo())
+
+	#extract only the top n; n here is 10
+	keywords=extract_topn_from_vector(feature_names, sorted_items, 100)
+	 
+	# # now print the results
+	# print("\n===Keywords===")
+	# for (w, s) in keywords:
+	#    print("%s=%.2f"%(w, s))
+	return list(set([ w for (w, s) in keywords]))
+
+
+
 @app.route("/questions/")
 def questions():
     
     num = request.args.get("num")
 
+    return_q = []
+    for idx in range(int(num)):
+        wrds = getJobFeature(fn_num=2)[:10]
+        return_q.append((idx, wrds))
+
+    return_q = dict(return_q)
     #input_vector = tfidf.transform([token(mstr)])
-    return "我要吐 %s 題數"%(num)
+    return "我要吐 %s 題數, 抓到的字詞：%s"%(num, return_q)
 
 
 if __name__ == '__main__':
