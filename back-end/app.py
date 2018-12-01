67a2284bbff9bfd04864f37afd96c2f00bd3d46b
AugChao <fychao.tw@gmail.com>
2018-12-01 11:11:28 +0800

    kmeans result 178

--- back-end/app.py
+++ back-end/app.py
@@ -1,11 +1,40 @@
 import flask
-
+import re
+import dill
+import jieba
+from sklearn.cluster import KMeans
+from sklearn.feature_extraction.text import TfidfVectorizer
 from flask import Flask
+
+# 載入模型檔
+tfidf = dill.load(open("model_tfidf.dil", 'rb'))
+kmeans = dill.load(open("model_kmeans.dil", 'rb'))
+
+
+def token(txt):
+    wrds = []
+    for wrd in jieba.cut(txt):
+        if re.match(u"[\u4e00-\u9fa5]+", wrd): #中文字
+            wrds.append(wrd)
+#         elif re.match("\d+", wrd): # 數字
+#             wrds.append(wrd)
+#         elif re.match("\w+", wrd): # 英文字
+#             wrds.append(wrd)
+        else:
+            pass
+    return wrds
+
+mstr = "AUG 使用的模式"
+input_vector = tfidf.transform([token(mstr)])
+kmeans.predict(input_vector)
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    mstr = "AUG 使用的模式"
+    input_vector = tfidf.transform([token(mstr)])
+    return "%s"%kmeans.predict(input_vector)
 
 
 if __name__ == '__main__':
